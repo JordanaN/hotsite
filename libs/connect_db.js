84132684552ce1ddab92b2ab3565fed370eefd85
Taynara Sene <tay.sene@gmail.com>
2016-11-10 11:15:41 -0200

    include dotenv

--- libs/connect_db.js
+++ libs/connect_db.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
+var dotenv = require('dotenv');
 var db;
+
+dotenv.load();
 
 module.exports = function() {
 	if(!db) {
-		db = mongoose.connect('mongodb://localhost/hotsite');
-		console.log('Conectado ao Mongo');		
+		db = mongoose.connect(process.env.DB_HOST);
+		console.log('Conectado ao Mongo');
 	}
 	return db;
 }
